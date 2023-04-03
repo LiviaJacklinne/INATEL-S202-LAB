00646fde8669eb5bf367656e318791946a9a8b70
Livia Jacklinne <99216080+LiviaJacklinne@users.noreply.github.com>
2023-04-03 15:25:41 -0300

    .

--- 4_Relatorio/main.py
+++ 4_Relatorio/main.py
@@ -5,34 +5,34 @@ db = Database(database="loja_de_roupas", collection="vendas")
 db.resetDatabase()
 
 # Questão 1
-# result1 = db.collection.aggregate([
-    ## Separa o array de produtos
-#     {"$unwind": "$produtos"},
-#     {"$group": {"_id":"$cliente_id", "total":{"$sum": {"$multiply": ["$produtos.quantidade","$produtos.preco"]}}}},
-    ## Encontra um item especifico
-#     {"$match":{"_id":"B"}}
-# ])
-# writeAJson(result1, "Total_cliente_B")
+result1 = db.collection.aggregate([
+    # Separa o array de produtos
+    {"$unwind": "$produtos"},
+    {"$group": {"_id":"$cliente_id", "total":{"$sum": {"$multiply": ["$produtos.quantidade","$produtos.preco"]}}}},
+    # Encontra um item especifico
+    {"$match":{"_id":"B"}}
+])
+writeAJson(result1, "Total_cliente_B")
 
 # Questão 2
-# result2 = db.collection.aggregate([
-#     {"$unwind": "$produtos"},
-#     {"$group": {"_id": "$produtos.nome", "total": {"$sum": "$produtos.quantidade"}}},
-    ## Ordena em ordem crescente
-#     {"$sort": {"total": 1}},
-    ## limita a uma exibição
-#     {"$limit": 1}
-# ])
-# writeAJson(result2, "Produto_menos_vendido")
+result2 = db.collection.aggregate([
+    {"$unwind": "$produtos"},
+    {"$group": {"_id": "$produtos.nome", "total": {"$sum": "$produtos.quantidade"}}},
+    # Ordena em ordem crescente
+    {"$sort": {"total": 1}},
+    # limita a uma exibição
+    {"$limit": 1}
+])
+writeAJson(result2, "Produto_menos_vendido")
 
 # Questão 3
-# result3 = db.collection.aggregate([
-#     {"$unwind":"$produtos"},
-#     {"$group": {"_id":"$cliente_id", "gasto":{"$sum":{"$multiply": ["$produtos.quantidade","$produtos.preco"]}}}},
-#     {"$sort": {"gasto": 1}},
-#     {"$limit":1}
-# ])
-# writeAJson(result3, "Cliente_menos_gastou")
+result3 = db.collection.aggregate([
+    {"$unwind":"$produtos"},
+    {"$group": {"_id":"$cliente_id", "gasto":{"$sum":{"$multiply": ["$produtos.quantidade","$produtos.preco"]}}}},
+    {"$sort": {"gasto": 1}},
+    {"$limit":1}
+])
+writeAJson(result3, "Cliente_menos_gastou")
 
 # Questão 4
 result4 = db.collection.aggregate([
